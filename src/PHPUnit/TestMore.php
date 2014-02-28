aec49496a753642acd4bfd8d805303229669831e
c9s <yoanlin93@gmail.com>
2014-02-28 12:55:02 +0800

    Add version tag

--- src/PHPUnit/TestMore.php
+++ src/PHPUnit/TestMore.php
@@ -4,6 +4,8 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * @VERSION 1.3.0
  */
 
 /* php 5.3.3 does not support this */
