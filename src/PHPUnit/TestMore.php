48ac47406bfd16188a9016cbee3e2a52100941d8
c9s <yoanlin93@gmail.com>
2014-02-28 12:55:50 +0800

    Checking in changes prior to tagging of version 1.3.1.

--- src/PHPUnit/TestMore.php
+++ src/PHPUnit/TestMore.php
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @VERSION 1.3.0
+ * @VERSION 1.3.1
  */
 
 /* php 5.3.3 does not support this */
